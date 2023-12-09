5ba62d61fb49aca12d08e56f2413efb3c8d7d420
Vlad Isan <vlad@innerbyte.uk>
2023-12-09 23:03:05 +0000

    part 1

--- puzzle.cpp
+++ puzzle.cpp
@@ -71,11 +71,29 @@ int puzzle_2(const std::string &base_file_path) {
 int do_puzzle_1(std::ifstream &file) {
     std::string line;
 
+    std::regex regex(R"((\b\w+\b) = \((\b\w+\b), (\b\w+\b)\))");
+
+    std::vector<Instruction> instructions;
+    NodeMap nodes;
+
+    std::getline(file, line);
+
+    std::for_each(line.begin(), line.end(), [&](char &c) {
+        instructions.emplace_back(c == 'R' ? Instruction::RIGHT : Instruction::LEFT);
+    });
+
     while (std::getline(file, line)) {
-        fmt::println("{}", line);
+        for (auto it = std::sregex_iterator(line.begin(), line.end(), regex); it != std::sregex_iterator(); ++it) {
+            const std::smatch &match = *it;
+
+            nodes[match.str(1)] = std::make_pair(match.str(2), match.str(3));
+        }
     }
 
-    return 0;
+    const std::string start = "AAA";
+    const std::string end = "ZZZ";
+
+    return get_steps_to_target(nodes, instructions, start, end);
 }
 
 int do_puzzle_2(std::ifstream &file) {
@@ -86,4 +104,39 @@ int do_puzzle_2(std::ifstream &file) {
     }
 
     return 0;
+}
+
+int get_steps_to_target(const NodeMap &node_map, std::vector<Instruction> &instructions, const std::string &start,
+                        const std::string &target) {
+    int steps = 0;
+
+    if (instructions.empty() || node_map.empty()) {
+        return steps;
+    }
+
+    std::string current = start;
+
+    evil_label_suck_it:
+
+    for (auto &instruction: instructions) {
+        const auto &node = node_map.at(current);
+
+        if (instruction == Instruction::LEFT) {
+            current = node.first;
+        } else {
+            current = node.second;
+        }
+
+        steps++;
+
+        if (current == target) {
+            break;
+        }
+    }
+
+    if (current != target) {
+        goto evil_label_suck_it;
+    }
+
+    return steps;
 }
--- puzzle.h
+++ puzzle.h
@@ -12,12 +12,21 @@
 #include <iostream>
 #include <map>
 #include <ranges>
+#include <regex>
 #include <string>
 #include <sstream>
 #include <unordered_map>
 #include <vector>
 
 #include "fmt/core.h"
+
+using NodeMap = std::unordered_map<std::string, std::pair<std::string, std::string>>;
+
+enum class Instruction {
+    LEFT,
+    RIGHT
+};
+
 
 int puzzle_sample_1(const std::string &base_file_path);
 
@@ -30,5 +39,7 @@ int puzzle_2(const std::string &base_file_path);
 int do_puzzle_1(std::ifstream &file);
 
 int do_puzzle_2(std::ifstream &file);
+
+int get_steps_to_target(const NodeMap &node_map, std::vector<Instruction> &instructions, const std::string &start, const std::string &target);
 
 #endif //PUZZLE_H
